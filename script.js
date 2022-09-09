c4863611f4c0e11be86aafe74bb168bf5b6eb415
Killa <102366446+KillaClick@users.noreply.github.com>
2022-09-09 15:55:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const board = document.querySelector("#board");
+const squaresNumber = 600;
+const colors = ["#32cd32", "#dc143c", "#ff1493", "#ffff00", "#00ffff"];
+
+for (let i = 0; i < squaresNumber; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => {
+    setColor(square);
+  });
+  square.addEventListener("mouseleave", () => {
+    removeColor(square);
+  });
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
